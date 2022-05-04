d387ba2435acf71a5d1c50a997c495d6e3297929
parnorma <parnorma@gmail.com>
2022-05-03 21:43:15 -0300

    aplicando estilos de la clase y practica 12

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ app.listen(PORT, () => {
     console.log("Servidor activo "+PORT);
 });
 // esto es un mildware para permitir que se puedan ver las imagenes
+// esto nos permite que podamos cargar las imagenes partiendo desde /
+// ejemplo /images/logo-mercado-liebre.svg  la barra representa localhost:3050
+// recomendable que siempre comience con barra siempre
 app.use(express.static(path.join(__dirname,"public")));
 
+//camino | path | ruta
 app.get('/',(req,res)=>{
     //funcion controladora
     res.sendFile(path.join(__dirname,"views/home.html"));
