1c5a5ea49b1d631ac606bc926e69b9517469b0c9
parnorma <parnorma@gmail.com>
2022-05-16 15:41:14 -0300

    terminando la practica de la clase 15

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ const app = express();
 const PORT = 3005;
 
 app.listen(PORT, () => {
-    console.log("Servidor activo "+PORT);
+  console.log("Servidor activo " + PORT);
 });
 // esto es un mildware para permitir que se puedan ver las imagenes
 // esto nos permite que podamos cargar las imagenes partiendo desde /
 // ejemplo /images/logo-mercado-liebre.svg  la barra representa localhost:3050
 // recomendable que siempre comience con barra siempre
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 //camino | path | ruta
-app.get('/',(req,res)=>{
-    //funcion controladora
-    res.sendFile(path.join(__dirname,"views/home.html"));
+app.get("/", (req, res) => {
+  //funcion controladora
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  //funcion controladora
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
+app.get("/login", (req, res) => {
+  //funcion controladora
+  res.sendFile(path.join(__dirname, "views/login.html"));
 });
